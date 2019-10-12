84f3fba218c1bd18f6d8614f39ab71eb3a0f8901
Marianne <t8kama06@students.oamk.fi>
2019-10-12 19:14:36 +0300

    Etusivun html runko valmis

---
+++ etusivu.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Etusivu</title>
+
+  </head>
+
+  <body>
+    <div id="topnav">
+      <ul>
+        <li><a href="etusivu.php">Virtuaaligalleria MATTE</a></li>
+        <li><a href="etusivu.php">Paikka taiteelle</a></li>
+        <li><a href="rekisteroidy.php">REKISTERÖIDY</a></li>
+        <li><a href="kirjaudu.php">KIRJAUDU SISÄÄN</a></li>
+      </ul>
+    </div>
+
+<div id="banner">
+  <img src="img/banner.png">
+</div>
+
+<div id="row">
+  <div id="column">
+    <h2><center>SYYSKUUN NÄYTTELY</center></h2>
+    <p>Syyskuuta tähdittää valokuvaaja parivaljakko...</p>
+    <p><center><a href="nayttelyt.php"><button>ASTU SISÄÄN</button></a></center></p>
+  </div>
+  <div id="column">
+    <img src="img/nattelyk.png">
+  </div>
+</div>
+tekstiä
+
+<div id="footer">
+  <li><a href="#">Yhteystiedot</a></li>
+  <li><a href="#">Usein kysyttyä</a></li>
+  <li><a href="#">Tietoa meistä</a></li>
+  <li><a href="#">Teoksesi meille?</a></li>
+  <li><a href="#">Yhteistyökumppanit</a></li>
+</div>
+  </body>
+</html>
